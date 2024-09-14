ccc552685ce5d4b5b7ad4e2f88f4523684e803d0
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-14 21:08:29 +0100

    Move CourseSection child components to CourseSection module

--- courseinfo/src/components/index.tsx
+++ courseinfo/src/components/index.tsx
@@ -1,5 +1,3 @@
 export { Header } from "./Header";
 export { Total } from "./Total";
-export { CourseContent } from "./CourseSection/CourseContent/CourseContent";
-export { Part } from "./CourseSection/CourseContent/Part";
-export { CourseSection } from "./CourseSection/CourseSection";
+export { CourseSection } from "./CourseSection";
