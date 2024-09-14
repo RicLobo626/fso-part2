eebbbc83744f41579edf6327f8446aaf35e4a2fd
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-15 00:49:16 +0100

    Create and style AlertBar component

--- phonebook/src/components/index.tsx
+++ phonebook/src/components/index.tsx
@@ -2,3 +2,4 @@ export { PersonForm } from "./PersonForm";
 export { PersonList } from "./PersonList";
 export { Filter } from "./Filter";
 export { Button } from "./Button";
+export { AlertBar } from "./AlertBar";
