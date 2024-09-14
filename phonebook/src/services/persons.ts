daaab53458d62efb536c3a32f48392a023e4d14a
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-14 18:50:34 +0100

    Add createPerson service

--- phonebook/src/services/persons.ts
+++ phonebook/src/services/persons.ts
@@ -1,9 +1,16 @@
 import axios from "axios";
+import { PersonFormValues } from "src/types";
 
 const baseURL = "/api/persons";
 
 export const getPersons = async () => {
   const { data } = await axios.get(baseURL);
+
+  return data;
+};
+
+export const createPerson = async (person: PersonFormValues) => {
+  const { data } = await axios.post(baseURL, person);
 
   return data;
 };
