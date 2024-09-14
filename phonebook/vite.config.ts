df784da428bbe76281fb3b684a192e22f77401ad
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-14 17:20:56 +0100

    Config server proxy

--- phonebook/vite.config.ts
+++ phonebook/vite.config.ts
@@ -9,4 +9,13 @@ export default defineConfig({
       src: "/src",
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
