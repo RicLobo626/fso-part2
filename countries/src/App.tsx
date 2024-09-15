80a1375c34b8a74dd9d6fd25291f4b2433494afd
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-15 18:46:35 +0100

    Add filter functionality

--- countries/src/App.tsx
+++ countries/src/App.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import { getCountries } from "src/services/countries";
+import { Country } from "src/types";
+import { FilterResults, Filter } from "src/components";
+
 function App() {
-  return <div>countries</div>;
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [filter, setFilter] = useState("");
+
+  const filteredCountries = countries.filter((country) => {
+    return country.name.common.toLowerCase().includes(filter.toLowerCase());
+  });
+
+  const isFiltering = filter.length > 0;
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await getCountries();
+        setCountries(data);
+      } catch (e) {
+        console.error("Failed to fetch countries:", e);
+      }
+    })();
+  }, []);
+
+  const handleFilter = (val: string) => setFilter(val);
+
+  return (
+    <>
+      <header>
+        <h1>Countries</h1>
+      </header>
+
+      <main>
+        <Filter onFilter={handleFilter} value={filter} />
+
+        {isFiltering && <FilterResults countries={filteredCountries} />}
+      </main>
+    </>
+  );
 }
 
 export default App;
